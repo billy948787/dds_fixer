5d82b41fe17e9321f4f65a90d664d18dcaca86f8
billy948787 <billy948787@gmail.com>
2024-12-21 21:56:28 +0800

    init commit

---
+++ dds_fixer.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import filedialog
+import threading
+from wand import image
+
+def convert_dds_file(path):
+    print(f"Converting {path}...")
+    with image.Image(filename = path) as img:
+        img.compression = "dxt1"
+        img.save(filename = path)
+    print(f"Converted {path}")
+    
+
+def convert_dds_files():
+
+    root = tk.Tk()
+    root.withdraw()
+    
+    file_paths = filedialog.askopenfilenames(
+        title="選取DDS檔案",
+        filetypes=[("DDS Files","*.dds")]
+    )
+
+    threads = []
+    for path in file_paths:
+        t = threading.Thread(target=convert_dds_file, args=(path,))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+
+if __name__ == "__main__":
+    convert_dds_files()
